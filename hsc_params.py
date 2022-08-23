32012e8ba5b40351a80fe759c8393ea2e4344f08
chuang2 <chuang2@ps004.is.localnet>
2022-08-23 23:45:19 +0200

    add smplx contact

--- hsc_params.py
+++ hsc_params.py
@@ -9,6 +9,11 @@ FRAME_ID = 150
 
 hsc_params = pickle.load(open(f'data/human_scene_contact/{SET}/{SEQ_NAME}/{FRAME_ID:05d}/{SUB_ID}.pkl', 'rb'))
 
-hsc_vert_id_smpl = np.where(hsc_params['contact'] > 0.)[0]          # (6890,): per-vertex 0/1 contact label in smpl format
+contact_labels = hsc_params['contact']                              # (6890,): per-vertex 0/1 contact label in smpl format
+hsc_vert_id_smpl = np.where(contact_labels > 0.)[0]          
 distplacement_vec_smplx = hsc_params['s2m_dist_id']                 # (10475,3): the vector that points from each vertex in smplx to the closest point on the scene scan
 closest_faces_on_scene_smplx = hsc_params['closest_triangles_id']   # (10475,3,3): the triangle which stores the point above
+
+import trimesh
+smplx_mesh = trimesh.load(f'data/human_scene_contact/{SET}/{SEQ_NAME}/{FRAME_ID:05d}/{SUB_ID}.obj', process=False)
+hsc_vert_id_smplx = np.where((smplx_mesh.visual.vertex_colors == (0, 255, 0, 255)).all(axis=1))[0]
